87f05e5d83df4d176f91b04f230053823b704b60
Jake VanderPlas <jakevdp@gmail.com>
2014-10-25 07:21:59 -0700

    finalize morning breakout

--- notebooks/fig_code/data.py
+++ notebooks/fig_code/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def linear_data_sample(N=40, rseed=0, m=3, b=-2):
     rng = np.random.RandomState(rseed)
 
@@ -8,7 +9,8 @@ def linear_data_sample(N=40, rseed=0, m=3, b=-2):
     y = m * x + b + dy * rng.randn(N)
 
     return (x, y, dy)
-    
+
+
 def linear_data_sample_big_errs(N=40, rseed=0, m=3, b=-2):
     rng = np.random.RandomState(rseed)
 
@@ -18,3 +20,28 @@ def linear_data_sample_big_errs(N=40, rseed=0, m=3, b=-2):
     y = m * x + b + dy * rng.randn(N)
 
     return (x, y, dy)
+
+
+def sample_light_curve(phased=True):
+    from astroML.datasets import fetch_LINEAR_sample
+    data = fetch_LINEAR_sample()
+    t, y, dy = data[18525697].T
+
+    if phased:
+        P_best = 0.580313015651
+        t /= P_best
+
+    return (t, y, dy)
+    
+
+def sample_light_curve_2(phased=True):
+    from astroML.datasets import fetch_LINEAR_sample
+    data = fetch_LINEAR_sample()
+    t, y, dy = data[10022663].T
+
+    if phased:
+        P_best = 0.61596079804
+        t /= P_best
+
+    return (t, y, dy)
+    
