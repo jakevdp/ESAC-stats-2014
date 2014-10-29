5610588d9e81b42d49bc60e9f5831c9e6348ae02
Jake VanderPlas <jakevdp@gmail.com>
2014-10-29 07:47:29 -0700

    add random forest lecture

--- notebooks/fig_code/figures.py
+++ notebooks/fig_code/figures.py
@@ -1,4 +1,6 @@
+import numpy as np
 import matplotlib.pyplot as plt
+
 
 def plot_venn_diagram():
     fig, ax = plt.subplots(subplot_kw=dict(frameon=False, xticks=[], yticks=[]))
@@ -9,3 +11,105 @@ def plot_venn_diagram():
     ax.text(0.7, 0.3, '$y$', size=30, ha='center', va='center')
     ax.text(0.0, 0.6, '$I$', size=30)
     ax.axis('equal')
+
+
+def plot_example_decision_tree():
+    fig = plt.figure(figsize=(10, 4))
+    ax = fig.add_axes([0, 0, 0.8, 1], frameon=False, xticks=[], yticks=[])
+    ax.set_title('Example Decision Tree: Animal Classification', size=24)
+
+    def text(ax, x, y, t, size=20, **kwargs):
+        ax.text(x, y, t,
+                ha='center', va='center', size=size,
+                bbox=dict(boxstyle='round', ec='k', fc='w'), **kwargs)
+
+    text(ax, 0.5, 0.9, "How big is\nthe animal?", 20)
+    text(ax, 0.3, 0.6, "Does the animal\nhave horns?", 18)
+    text(ax, 0.7, 0.6, "Does the animal\nhave two legs?", 18)
+    text(ax, 0.12, 0.3, "Are the horns\nlonger than 10cm?", 14)
+    text(ax, 0.38, 0.3, "Is the animal\nwearing a collar?", 14)
+    text(ax, 0.62, 0.3, "Does the animal\nhave wings?", 14)
+    text(ax, 0.88, 0.3, "Does the animal\nhave a tail?", 14)
+
+    text(ax, 0.4, 0.75, "< 1m", 12, alpha=0.4)
+    text(ax, 0.6, 0.75, "> 1m", 12, alpha=0.4)
+
+    text(ax, 0.21, 0.45, "yes", 12, alpha=0.4)
+    text(ax, 0.34, 0.45, "no", 12, alpha=0.4)
+
+    text(ax, 0.66, 0.45, "yes", 12, alpha=0.4)
+    text(ax, 0.79, 0.45, "no", 12, alpha=0.4)
+
+    ax.plot([0.3, 0.5, 0.7], [0.6, 0.9, 0.6], '-k')
+    ax.plot([0.12, 0.3, 0.38], [0.3, 0.6, 0.3], '-k')
+    ax.plot([0.62, 0.7, 0.88], [0.3, 0.6, 0.3], '-k')
+    ax.plot([0.0, 0.12, 0.20], [0.0, 0.3, 0.0], '--k')
+    ax.plot([0.28, 0.38, 0.48], [0.0, 0.3, 0.0], '--k')
+    ax.plot([0.52, 0.62, 0.72], [0.0, 0.3, 0.0], '--k')
+    ax.plot([0.8, 0.88, 1.0], [0.0, 0.3, 0.0], '--k')
+    ax.axis([0, 1, 0, 1])
+
+
+def visualize_tree(estimator, X, y, boundaries=True,
+                   xlim=None, ylim=None):
+    estimator.fit(X, y)
+
+    if xlim is None:
+        xlim = (X[:, 0].min() - 0.1, X[:, 0].max() + 0.1)
+    if ylim is None:
+        ylim = (X[:, 1].min() - 0.1, X[:, 1].max() + 0.1)
+
+    x_min, x_max = xlim
+    y_min, y_max = ylim
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    Z = estimator.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, alpha=0.2, cmap='rainbow')
+    plt.clim(y.min(), y.max())
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='rainbow')
+    plt.axis('off')
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)        
+    plt.clim(y.min(), y.max())
+    
+    # Plot the decision boundaries
+    def plot_boundaries(i, xlim, ylim):
+        if i < 0:
+            return
+
+        tree = estimator.tree_
+        
+        if tree.feature[i] == 0:
+            plt.plot([tree.threshold[i], tree.threshold[i]], ylim, '-k')
+            plot_boundaries(tree.children_left[i],
+                            [xlim[0], tree.threshold[i]], ylim)
+            plot_boundaries(tree.children_right[i],
+                            [tree.threshold[i], xlim[1]], ylim)
+        
+        elif tree.feature[i] == 1:
+            plt.plot(xlim, [tree.threshold[i], tree.threshold[i]], '-k')
+            plot_boundaries(tree.children_left[i], xlim,
+                            [ylim[0], tree.threshold[i]])
+            plot_boundaries(tree.children_right[i], xlim,
+                            [tree.threshold[i], ylim[1]])
+            
+    if boundaries:
+        plot_boundaries(0, plt.xlim(), plt.ylim())
+
+
+def plot_tree_visualization(X, y):
+    from sklearn.tree import DecisionTreeClassifier
+
+    def interactive_tree(depth):
+        clf = DecisionTreeClassifier(max_depth=depth, random_state=0)
+        visualize_tree(clf, X, y)
+
+    from IPython.html.widgets import interact
+    return interact(interactive_tree, depth=[1, 5])
