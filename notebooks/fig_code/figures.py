a36337525e9385e84a29cc0f519169838cd82597
Jake VanderPlas <jakevdp@gmail.com>
2014-10-30 14:23:26 -0700

    update KMeans example

--- notebooks/fig_code/figures.py
+++ notebooks/fig_code/figures.py
@@ -113,3 +113,63 @@ def plot_tree_interactive(X, y):
 
     from IPython.html.widgets import interact
     return interact(interactive_tree, depth=[1, 5])
+
+
+def plot_kmeans_interactive():
+    from IPython.html.widgets import interact
+    from sklearn.metrics.pairwise import euclidean_distances
+    from sklearn.datasets.samples_generator import make_blobs
+    
+    X, y = make_blobs(n_samples=300, centers=4,
+                      random_state=0, cluster_std=0.60)
+
+    def _kmeans_step(frame, n_clusters):
+        rng = np.random.RandomState(2)
+        labels = np.zeros(X.shape[0])
+        centers = rng.randn(n_clusters, 2)
+
+        nsteps = frame // 3
+
+        for i in range(nsteps + 1):
+            old_centers = centers
+            if i < nsteps or frame % 3 > 0:
+                dist = euclidean_distances(X, centers)
+                labels = dist.argmin(1)
+
+            if i < nsteps or frame % 3 > 1:
+                centers = np.array([X[labels == j].mean(0)
+                                    for j in range(n_clusters)])
+                nans = np.isnan(centers)
+                centers[nans] = old_centers[nans]
+
+
+        # plot the cluster centers
+        plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='rainbow');
+        plt.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
+                    c=np.arange(n_clusters),
+                    s=200, cmap='rainbow')
+        plt.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
+                    c='black', s=50)
+
+        # plot new centers if third frame
+        if frame % 3 == 2:
+            for i in range(n_clusters):
+                plt.annotate('', centers[i], old_centers[i], 
+                             arrowprops=dict(arrowstyle='->', linewidth=1))
+            plt.scatter(centers[:, 0], centers[:, 1], marker='o',
+                        c=np.arange(n_clusters),
+                        s=200, cmap='rainbow')
+            plt.scatter(centers[:, 0], centers[:, 1], marker='o',
+                        c='black', s=50)
+
+        plt.xlim(-4, 4)
+        plt.ylim(-2, 10)
+
+        if frame % 3 == 1:
+            plt.text(3.8, 9.5, "1. Reassign points to nearest centroid",
+                     ha='right', va='top', size=14)
+        elif frame % 3 == 2:
+            plt.text(3.8, 9.5, "2. Update centroids to cluster means",
+                     ha='right', va='top', size=14)
+
+    return interact(_kmeans_step, frame=[0, 50], n_clusters=[3, 5])
